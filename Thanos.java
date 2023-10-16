ebc95630790f5999fe2fdea8bc5d4e97f06403f0
Emma Rosso <emmarosso.pro@gmail.com>
2023-10-16 16:42:44 +0200

    List quest

--- Thanos.java
+++ Thanos.java
@@ -5,6 +5,7 @@ public class Thanos {
     public static void main(String[] args) {
 
         // TODO 1 : Create an empty heroes list
+        ArrayList<Hero> heroes = new ArrayList<>();
 
         // TODO 2 : Add those heroes to the list
         // name: Black Widow, age: 34
@@ -15,13 +16,25 @@ public class Thanos {
         // name: Thor, age: 1500
         // name: Hulk, age: 49
         // name: Doctor Strange, age: 42
+        heroes.add(new Hero("Black Widow", 34));
+        heroes.add(new Hero("Captain America", 100));
+        heroes.add(new Hero("Vision", 3));
+        heroes.add(new Hero("Iron Man", 48));
+        heroes.add(new Hero("Scarlet Witch", 29));
+        heroes.add(new Hero("Thor", 1500));
+        heroes.add(new Hero("Hulk", 49));
+        heroes.add(new Hero("Doctor Strange", 42));
 
         // TODO 3 : It's Thor birthday, now he's 1501
-
+        heroes.get(5).setAge(1501);
         // TODO 4 : Shuffle the heroes list
-
+        Collections.shuffle(heroes);
         // TODO 5 : Keep only the half of the list
+        List<Hero> halfList = heroes.subList(0, 4);
 
         // TODO 6 : Loop throught the list and display the name of the remaining heroes
+        for (Hero hero: halfList) {
+          System.out.println(hero.getName());
+        };
     }
 }
